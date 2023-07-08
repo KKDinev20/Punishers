ff53bfc41d8eb0cec640a78795c199d71dd42078
PSStefanov19 <PSStefanov19@codingburgas.bg>
2023-07-08 10:13:30 +0300

    Change hashing algorithm to BCrypt

--- Application/BussinessLogicLayer/Hashing.cs
+++ Application/BussinessLogicLayer/Hashing.cs
@@ -4,20 +4,18 @@ using System.Text;
 
 namespace BussinessLogicLayer
 {
-	public static class Hashing
-	{
-		public static string ToSHA256(string s)
-		{
-			using var sha256 = SHA256.Create();
-			byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(s));
+    using BC = BCrypt.Net.BCrypt;
+    public static class Hashing
+    {
+        public static string hashPassowrd(string unhashedPassword)
+        {
+            return BC.HashPassword(unhashedPassword);
+        }
 
-			var sb = new StringBuilder();
-			for (int i = 0; i < bytes.Length; i++)
-			{
-				sb.Append(bytes[i].ToString("x2"));
-			}
-			return sb.ToString();
-		}
-	}
+        public static bool validatePassword(string unhashedPassword, string passwordHash)
+        {
+            return BC.Verify(unhashedPassword, passwordHash);
+        }
+    }
 }
 
--- Application/Start/Program.cs
+++ Application/Start/Program.cs
@@ -12,26 +12,7 @@ namespace Program
     {
         public static void Main(string[] args)
         {
-            AibestdbContext context = new AibestdbContext();
-
-            List<User> users = UserRepository.GetAllUsers();
-
-            int id = 0;
-            string username = Console.ReadLine();
-            string email = Console.ReadLine();
-            string password = Console.ReadLine();
-            password = Hashing.ToSHA256(password);
-
-            Console.WriteLine(password);
-            //User user = new User(id, username, email, password);
-
-           // Register.RegisterUser(id, username, email, password);
-            //users.Add(user);
-
-            //foreach (var item in users) 
-            //{
-            //    Console.WriteLine($"{item.Username}, {item.Id}, {item.Password}");
-            //}
+            Console.WriteLine(Hashing.hashPassowrd("pass1234"));
         }
     }
 }
