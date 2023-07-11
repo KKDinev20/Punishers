5066d7d5fc05cd57a8118247d2f526872deaa1b4
PSStefanov19 <PSStefanov19@codingburgas.bg>
2023-07-12 00:38:40 +0300

    Add Get and Post requests for /resume

---
+++ Application/WebApi/Controllers/ResumeController.cs
@@ -0,0 +1,23 @@
+﻿using DataAccessLayer.Models;
+using DataAccessLayer.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("resumes")]
+    public class ResumeController : Controller
+    {
+        [HttpPost(Name = "PostResumes")]
+        public void Post(Resume resume) 
+        {
+            ResumeRepository.CreateResume(resume);
+        }
+
+        [HttpGet(Name = "{id}")]
+        public Resume Get(int id) 
+        {
+            return ResumeRepository.GetResumeById(id);
+        }
+    }
+}
